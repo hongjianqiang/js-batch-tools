c1a78921d856c84be8c3e0bc9765e90bb00fd84f
hongjianqiang <569250030@qq.com>
2020-01-23 19:57:37 +0800

    修改了Babel配置

--- .babelrc.js
+++ .babelrc.js
@@ -1,15 +1,16 @@
 module.exports = function(api) {
-    console.log(`Version: v${api.version}`);
+    console.log(`Version: v${api.version}\n`);
 
     api.cache(true);
 
     return {
         presets: [
-            '@babel/preset-env'
+            // '@babel/preset-env'
         ],
 
         plugins: [
             // './plugins/accurate-operator'
+            './plugins/operator-reload'
         ]
     };
 }
